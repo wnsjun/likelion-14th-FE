ef64db205d4113c7913ef58ab4fed17042cd70a8
wnsjun <shadd3338@gmail.com>
2026-02-04 15:28:37 +0000

    #40 [FEAT] 홈페이지-지난 기수 프로젝트 보기 라우팅 설정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
 
           <div className="flex flex-col sm:flex-row gap-[20px] justify-center items-center mb-[80px]">
             <SeeMore text="모집 안내 사항 바로가기" navi={'/recruiting'} />
-            <SeeMore text="지난 기수 프로젝트 바로가기" />
+            <SeeMore text="지난 기수 프로젝트 바로가기" navi={'/archive'} />
           </div>
         </div>
       </section>
