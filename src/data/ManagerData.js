282e3991bdf55e15a714b885b1be8ad49e8f329f
wnsjun <shadd3338@gmail.com>
2026-02-04 14:29:30 +0000

    [FIX] 사진 경로 수정

--- src/data/ManagerData.js
+++ src/data/ManagerData.js
@@ -1,3 +1,13 @@
+import 황신애 from '../../src/assets/photo/황신애.jpeg';
+import 이윤서 from '../../src/assets/photo/이윤서.jpeg';
+import 김준희 from '../../src/assets/photo/김준희.jpeg';
+import 이정인 from '../../src/assets/photo/이정인.jpeg';
+import 강채원 from '../../src/assets/photo/강채원.jpeg';
+import 고다현 from '../../src/assets/photo/고다현.jpeg';
+import 최우혁 from '../../src/assets/photo/최우혁.jpeg';
+import 박서영 from '../../src/assets/photo/박서영.jpeg';
+import 윤서현 from '../../src/assets/photo/윤서현.jpeg';
+
 export const MANAGER_DATA = [
   {
     id: 1,
@@ -5,62 +15,62 @@ export const MANAGER_DATA = [
     part: '회장',
     major: '컴퓨터공학과 22',
     phone: '010-3120-2936',
-    imgSrc: '../src/assets/photo/황신애.jpeg',
+    imgSrc: 황신애,
   },
   {
     id: 2,
     name: '이윤서',
     part: '프론트엔드',
     major: '컴퓨터공학과 23',
-    imgSrc: '../src/assets/photo/이윤서.jpeg',
+    imgSrc: 이윤서,
   },
   {
     id: 3,
     name: '김준희',
     part: '프론트엔드',
     major: '컴퓨터공학과 22',
-    imgSrc: '../src/assets/photo/김준희.jpeg',
+    imgSrc: 김준희,
   },
   {
     id: 4,
     name: '이정인',
     part: '프론트엔드',
     major: '컴퓨터공학과 22',
-    imgSrc: '../src/assets/photo/이정인.jpeg',
+    imgSrc: 이정인,
   },
   {
     id: 5,
     name: '강채원',
     part: '기획/디자인',
     major: '시각디자인과 22',
-    imgSrc: '../src/assets/photo/강채원.jpeg',
+    imgSrc: 강채원,
   },
   {
     id: 6,
     name: '고다현',
     part: '기획/디자인',
     major: '시각디자인과 22',
-    imgSrc: '../src/assets/photo/고다현.jpeg',
+    imgSrc: 고다현,
   },
   {
     id: 7,
     name: '최우혁',
     part: '백엔드',
     major: '컴퓨터공학과 23',
-    imgSrc: '../src/assets/photo/최우혁.jpeg',
+    imgSrc: 최우혁,
   },
   {
     id: 8,
     name: '박서영',
     part: '백엔드',
     major: '컴퓨터공학과 21',
-    imgSrc: '../src/assets/photo/박서영.jpeg',
+    imgSrc: 박서영,
   },
   {
     id: 9,
     name: '윤서현',
     part: '백엔드',
     major: '컴퓨터공학과 23',
-    imgSrc: '../src/assets/photo/윤서현.jpeg',
+    imgSrc: 윤서현,
   },
 ];
