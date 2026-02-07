3e521240b22b01645ed25733e28dc4766c590c25
wnsjun <shadd3338@gmail.com>
2026-02-07 18:19:04 +0000

    #85 [CHORE] 오탈자 수정

--- src/components/home/Introduction.jsx
+++ src/components/home/Introduction.jsx
@@ -8,7 +8,7 @@ const Introduction = () => {
           Growl to World
         </div>
         <div className="sm:text-[20px] sm:font-semibold text-[16px] font-semibold flex text-center text-black">
-          멋쟁이 사자처럼 대학은 국내외 121개 대학,
+          멋쟁이사자처럼 대학은 국내외 121개 대학,
           <br className="sm:hidden block" /> 4천여명이 함께하는
           <br className="lg:hidden block" />
           국내 최대 규모 IT 창업 동아리입니다
